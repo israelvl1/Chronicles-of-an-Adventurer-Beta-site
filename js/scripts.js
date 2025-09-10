2cfa1dfb2885b8868fa43c98030b4cde75fab664
israelvl1 <157428088+israelvl1@users.noreply.github.com>
2025-09-10 09:24:21 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -54,7 +54,7 @@ class Particle {
 
 // Cria imagem de onde as partículas serão geradas
 const img = new Image();
-img.src = 'fundos/dinamic.png'; // Caminho da imagem
+img.src = 'https://israelvl1.github.io/Chronicles-of-an-Adventurer-Beta-site/fundos/dinamic.png'; // Caminho da imagem
 
 // Cria partículas com base na imagem carregada
 function createParticlesFromImage() {
